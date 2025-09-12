b0bf5510a2994f3108f281a820671f5094af13e7
mkheirkh <mahdikheirkhah060@gmail.com>
2025-09-13 00:39:29 +0300

    set up basic home page

--- Angul-It/src/app/components/home/home.ts
+++ Angul-It/src/app/components/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // 1. Import the Router
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
 
+  // 2. Inject the Router service in the constructor
+  constructor(private router: Router) {}
+
+  // 3. Create the method that will be called on button click
+  startChallenge(): void {
+    this.router.navigate(['/captcha']);
+  }
 }
